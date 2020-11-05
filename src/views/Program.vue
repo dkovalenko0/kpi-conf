35cb3377245fd6ed7b42d336975b28f77f80e922
dkovalenko0 <dimakovalenko715@gmail.com>
2020-11-05 15:27:00 +0200

    add program page

--- src/views/Program.vue
+++ src/views/Program.vue
@@ -1,6 +1,359 @@
 <template>
-  <div></div>
+  <section class="programs">
+    <div class="container">
+      <div class="programs__content">
+        <div class="title__container">
+          <h2 class="title--gray">Program</h2>
+        </div>
+
+        <div class="home__banner">
+          <div class="banner__inner">
+            <div class="banner__pretitle">6rd International Conference</div>
+            <div class="banner__title">High Performance Computing (HPC-UA 2020)</div>
+            <div class="banner__date">November 06 - 07, 2020 Kyiv / Ukraine</div>
+          </div>
+        </div>
+
+        <p class="text center">Faculty of Informatics and Computer Science of Igor Sikorsky Kyiv Polytechnic Institute</p>
+        <p class="text center">V.M. Glushkov Institute of Cybernetics of NAS of Ukraine</p>
+        <p class="text center">GlobalLogic Ukraine</p>
+
+        <div class="banner__title center date">November 6, 2020 </div>
+
+        <ul class="program__list">
+          <li class="program__item">
+            <span class="time">9:00</span>
+            <div>
+              <span class="name">Opening of the conference</span>
+              <span class="university">Head of Department of Computer Engineering</span>
+              <span class="author">Sergiy Stirenko</span>
+            </div>
+          </li>
+        </ul>
+
+        <h3 class="program__title cloud-computing__title center">Cloud Computing Section</h3>
+
+        <ul class="program__list">
+          <li class="program__item">
+            <span class="time">9:15</span>
+              <div>
+                <span class="name">Optimizing Kubernetes Resource Requests</span>
+                <span class="university">Igor Sikorsky Kyiv Polytechnic Institute</span>
+                <span class="author">Oleksandr Kovalchuk</span>
+              </div>
+          </li>
+        </ul>
+
+        <h3 class="program__title deep-learning__title center">Deep Learning Section</h3>
+
+        <ul class="program__list">
+          <li class="program__item">
+            <span class="time">9:30</span>
+            <div>
+              <span class="name">On Biomedical Computations in Cluster and Cloud Environment</span>
+              <span class="university">V.M. Glushkov Institute of Cybernetics of NAS of Ukraine</span>
+              <span class="author">Tamara Bardadym</span>
+            </div>
+          </li>
+          <li class="program__item">
+            <span class="time">9:45</span>
+            <div>
+              <span class="name">Comparative Analysis of the Datasets with Multimodal Content</span>
+              <span class="university">Igor Sikorsky Kyiv Polytechnic Institute</span>
+              <span class="author">Maksym Shulha</span>
+            </div>
+          </li>
+          <li class="program__item">
+            <span class="time">10:00</span>
+            <div>
+              <span class="name">Blockchain and Smart Contracts for Provenance of Deep Learning Content in Healthcare</span>
+              <span class="university">Igor Sikorsky Kyiv Polytechnic Institute</span>
+              <span class="author">Volodymyr Valko </span>
+            </div>
+          </li>
+          <li class="program__item">
+            <span class="time">10:15</span>
+            <div>
+              <span class="name">Adaptive Threat Detection Method Based on the Operating System Audit Subsystem</span>
+              <span class="university">Igor Sikorsky Kyiv Polytechnic Institute</span>
+              <span class="author">Anna Verner</span>
+            </div>
+          </li>
+        </ul>
+
+        <h3 class="program__title information__title center">Information Systems</h3>
+
+        <ul class="program__list">
+          <li class="program__item">
+            <span class="time">10:45</span>
+            <div>
+              <span class="name">Sparse Signal Reconstruction via Kanerva's Sparse Distributed Memory</span>
+              <span class="university">V.M. Glushkov Institute of Cybernetics of NAS of Ukraine</span>
+              <span class="author">Ruslan Vdovychenko</span>
+            </div>
+          </li>
+          <li class="program__item">
+            <span class="time">11:00</span>
+            <div>
+              <span class="name">Preconditions for Ukraine’s integration into the EU Digital Single Market</span>
+              <span class="university">V.M. Glushkov Institute of Cybernetics of NAS of Ukraine</span>
+              <span class="author">Anastasiia Hrechko</span>
+            </div>
+          </li>
+        </ul>
+
+        <h3 class="program__title software__title center">Software and Applications</h3>
+
+        <ul class="program__list">
+          <li class="program__item">
+            <span class="time">11:15</span>
+            <div>
+              <span class="name">Automated Parallelization of Big Data Processing on Multiple GPUs</span>
+              <span class="university">Igor Sikorsky Kyiv Polytechnic Institute</span>
+              <span class="author">Oleksii Beketov</span>
+            </div>
+          </li>
+          <li class="program__item">
+            <span class="time">11:30</span>
+            <div>
+              <span class="name">Epidemiological trends: models and software</span>
+              <span class="university">V.M. Glushkov Institute of Cybernetics of NAS of Ukraine</span>
+              <span class="author">Maksym Dunaievskyi</span>
+            </div>
+          </li>
+          <li class="program__item">
+            <span class="time">11:45</span>
+            <div>
+              <span class="name">Parallel computing of elastic magnetic systems at the nanoscale</span>
+              <span class="university">Taras Shevchenko University of Kyiv</span>
+              <span class="author">Artem Tomilo</span>
+            </div>
+          </li>
+          <li class="program__item">
+            <span class="time">12:00</span>
+            <div>
+              <span class="name">Accelerating of Program Execution by Discrete Particle Swarm Optimization Method</span>
+              <span class="university">Pukhov Institute for Modeling in Energy Engineering</span>
+              <span class="author">Sergii Sushko</span>
+            </div>
+          </li>
+          <li class="program__item">
+            <span class="time">12:15</span>
+            <div>
+              <span class="name">A Method of Recognition Technology of Fake New</span>
+              <span class="university">Igor Sikorsky Kyiv Polytechnic Institute</span>
+              <span class="author">Liudmyla Mishchenko</span>
+            </div>
+          </li>
+          <li class="program__item">
+            <span class="time">12:30</span>
+            <div>
+              <span class="name">Multimodal Data Integration and Processing Method for Brain-Computer Interface Research</span>
+              <span class="university">Igor Sikorsky Kyiv Polytechnic Institute</span>
+              <span class="author">Kostiantyn Kostiukevych</span>
+            </div>
+          </li>
+        </ul>
+
+        <h3 class="program__title embedded__title center">Embedded Systems and Robotics</h3>
+
+        <ul class="program__list">
+          <li class="program__item">
+            <span class="time">14:00</span>
+            <div>
+              <span class="name">Turning simple microcontroller board into IoT platform</span>
+              <span class="university">GlobalLogic Ukraine</span>
+              <span class="author">Viktoriia Taraniuk</span>
+            </div>
+          </li>
+          <li class="program__item">
+            <span class="time">14:45</span>
+            <div>
+              <span class="name">Feasibility Study Of Hardware Acceleration In Gif Compression Algorithm</span>
+              <span class="university">Igor Sikorsky Kyiv Polytechnic Institute</span>
+              <span class="author">Ivan Mozghoviy</span>
+            </div>
+          </li>
+          <li class="program__item">
+            <span class="time">15:00</span>
+            <div>
+              <span class="name">Try-on AR</span>
+              <span class="university">GlobalLogic Ukraine</span>
+              <span class="author">Pavlo Didyk</span>
+            </div>
+          </li>
+          <li class="program__item">
+            <span class="time">15:30</span>
+            <div>
+              <span class="name">Genetic Programming of Discrete Cosine Transform Processors</span>
+              <span class="university">Igor Sikorsky Kyiv Polytechnic Institute</span>
+              <span class="author">Anatoliy Sergiyenko</span>
+            </div>
+          </li>
+          <li class="program__item">
+            <span class="time">15:45</span>
+            <div>
+              <span class="name">No Title</span>
+              <span class="university">GlobalLogic Ukraine</span>
+              <span class="author">Olga Kurna</span>
+            </div>
+          </li>
+          <li class="program__item">
+            <span class="time">16:00</span>
+            <div>
+              <span class="name">Traffic engineering in DNC with a ramified topology</span>
+              <span class="university">Igor Sikorsky Kyiv Polytechnic Institute</span>
+              <span class="author">Dmytro Korenko</span>
+            </div>
+          </li>
+          <li class="program__item">
+            <span class="time">16:15</span>
+            <div>
+              <span class="name">Pit searching system using iot technology for smartcity</span>
+              <span class="university">Igor Sikorsky Kyiv Polytechnic Institute</span>
+              <span class="author">Anton Kopiika</span>
+            </div>
+          </li>
+          <li class="program__item">
+            <span class="time">16:30</span>
+            <div>
+              <span class="name">The data transmission system based on network communication protocols for IoT</span>
+              <span class="university">Igor Sikorsky Kyiv Polytechnic Institute</span>
+              <span class="author">Serhiy Nikolskiy, Sergiy Vanyarha</span>
+            </div>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+  </section>
 </template>
 
 <style lang="scss">
+
+.program__list {
+  margin-top: 35px;
+  margin-left: 15%;
+  margin-right: 15%;
+
+  @media (max-width: 468px) {
+    margin-left: 5%;
+    margin-right: 5%;
+  }
+}
+
+.program__item {
+  margin-top: 35px;
+  display: flex;
+}
+
+.time {
+  margin-right: 35px;
+  font-weight: 600;
+}
+
+.name {
+  display: block;
+  margin-bottom: 15px;
+  font-weight: 600;
+}
+
+.university {
+  display: block;
+  margin-bottom: 15px;
+  font-weight: 600;
+  font-size: 14px;
+}
+
+.author {
+  font-style: italic;
+}
+
+.center {
+  text-align: center;
+}
+
+.text {
+  margin-top: 10px;
+}
+
+.date {
+  margin-top: 55px;
+  margin-bottom: 100px;
+}
+
+.program__title {
+  margin-top: 55px;
+  font-size: 38px;
+  font-weight: 600;
+  line-height: 1.5;
+}
+
+.cloud-computing__title {
+  color: #4ead26;
+}
+
+.deep-learning__title {
+  color: #bf1313;
+}
+
+.software__title {
+  color: #0a93d3;
+}
+
+.embedded__title {
+  color: #ed9625;
+}
+
+.information__title {
+  color: rebeccapurple;
+}
+
+
+.programs__content {
+  padding: 40px 15px;
+  background-color: #fff;
+  color: #000;
+
+  .title__container {
+    display: flex;
+    justify-content: center;
+  }
+}
+
+.home__banner {
+  margin-bottom: 35px;
+  width: 100%;
+
+  color: #000;
+  line-height: 50px;
+  font-weight: 400;
+  text-align: center;
+}
+
+.banner__inner {
+  padding-top: 55px;
+
+  @media (max-width: 468px) {
+    padding-top: 50px;
+  }
+}
+
+.banner__pretitle {
+  font-size: 22px;
+}
+
+.banner__title {
+  font-size: 34px;
+  font-weight: 600;
+}
+
+.banner__posttitle {
+  font-size: 32px;
+  font-weight: 600;
+}
+
+.banner__date {
+  font-size: 26px;
+}
 </style>
